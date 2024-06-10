0dca0c03b9e98e40da12d4487496737e419581b1
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2024-06-09 23:10:48 -0300

    Update TestCreateBinaryTreeDriver.cpp

--- TestCreateBinaryTreeDriver.cpp
+++ TestCreateBinaryTreeDriver.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
-#include "MainHeader/cpa.h"
 #include "BinaryTree/BinaryTree.h"
 
 using namespace std;
-using namespace cpa;
 using namespace bt;
 
 // tem que chamar as funções que cronometra os tempos de criação de arvores
 int main()
 {
-    //Node<int>* root = nullptr;
-    int iLength = 10000; // 10,000 elementos
-    int iNumTests = 100; // 100 listas
-
-    cout << "Measuring generating time for trees: " << endl;
-    measureGenerationTime<int>(generateRandomTree, iLength, iNumTests, "TreeGeneratingTimes.txt");
-
-    cout << "Measuring search time for DFS: " << endl;
-    measureSearchTime<int>(searchNodeDFS, generateRandomTree, iLength, iNumTests, "dfs_tree_search_times.txt");
-
-    cout << "Measuring search time for BFS: " << endl;
-    measureSearchTime<int>(bfsTraversal, generateRandomTree, iLength, iNumTests, "bfs_tree_search_times.txt");
-
-
+    Node<int>* root = nullptr;
+    int numNodes = 10000;
+    int numTrees = 100;
+    
+    measureBinaryTreeCreation(generateRandomTree,numNodes, numTrees, "CreateBinaryTree");
     return 0;
 }
