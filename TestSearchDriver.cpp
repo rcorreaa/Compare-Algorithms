f9294c002987000059fcdd8b0cd603d23c6ae535
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2024-06-09 23:11:20 -0300

    Update TestSearchDriver.cpp

--- TestSearchDriver.cpp
+++ TestSearchDriver.cpp
@@ -5,8 +5,17 @@ using namespace std;
 using namespace bt;
 
 // tem que chamar as funções que cronometra os tempos das buscas
-int main()
-{
-    Node<int>* root = nullptr;
+int main() {
+    int numNodes = 100000; // 100.000 nodes para melhor analise
+    int numTrees = 100;
+    std::string filename = "bfs_benchmark_results.txt";
+
+    // Cronometrar o método BFS
+    benchmarkSearchMethod(bfsTraversal<int>, numNodes, numTrees, filename);
+
+    // Cronometrar o método DFS
+    filename = "dfs_benchmark_results.txt";
+    benchmarkSearchMethod(searchNodeDFS<int>, numNodes, numTrees, filename);
+
     return 0;
 }
