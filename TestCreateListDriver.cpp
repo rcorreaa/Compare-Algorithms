30929c154da05f42638bcf6f9bd994ed8ea9c73b
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2024-06-09 23:11:05 -0300

    Update TestCreateListDriver.cpp

--- TestCreateListDriver.cpp
+++ TestCreateListDriver.cpp
@@ -7,7 +7,7 @@ using namespace cpa;
 // deve ter a função que cronometra o tempo pra criação de listas
 int main()
 {
-
+    measureDLLCreation<int>(generateRandomList,10000,100,"CreateDLL");
 
     return 0;
 }
